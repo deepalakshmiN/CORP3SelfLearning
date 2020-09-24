b12402f6cf9e7227143cb168c2604ab3bf8accc9
deepalakshmin <deepalakshmi.nakeeran@gmail.com>
2020-09-23 17:13:39 -0700

    Code First Approach with Entity Framework in MVC

---
+++ SqlCodeFirst/Context/BlogContext.cs
@@ -0,0 +1,16 @@
+﻿using SqlCodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SqlCodeFirst.Context
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("testCsqldb") { }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
