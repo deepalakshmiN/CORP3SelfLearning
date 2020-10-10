8e0d722ea8f223fb599a907d6a638dfe836581e0
deepalakshmin <deepalakshmi.nakeeran@gmail.com>
2020-10-09 18:22:02 -0700

    added changes

--- RegistrationAndLogin/RegistrationAndLogin/Controllers/AccountController.cs
+++ RegistrationAndLogin/RegistrationAndLogin/Controllers/AccountController.cs
@@ -25,17 +25,12 @@ namespace RegistrationAndLogin.Controllers
         [HttpPost]
         public ActionResult SaveRegisterDetails(Register registerDetails)
         {
-            //We check if the model state is valid or not. We have used DataAnnotation attributes.
-            //If any form value fails the DataAnnotation validation the model state becomes invalid.
             if (ModelState.IsValid)
             {
-                //create database context using Entity framework 
                 using (var databaseContext = new SqlCodeFirstMvcDbEntities())
                 {
-                    //If the model state is valid i.e. the form values passed the validation then we are storing the User's details in DB.
                     RegisterUser reglog = new RegisterUser();
 
-                    //Save all details in RegitserUser object
 
                     reglog.FirstName = registerDetails.FirstName;
                     reglog.LastName = registerDetails.LastName;
@@ -43,7 +38,6 @@ namespace RegistrationAndLogin.Controllers
                     reglog.Password = registerDetails.Password;
 
 
-                    //Calling the SaveDetails method which saves the details.
                     databaseContext.RegisterUsers.Add(reglog);
                     databaseContext.SaveChanges();
                 }
@@ -54,7 +48,6 @@ namespace RegistrationAndLogin.Controllers
             else
             {
 
-                //If the validation fails, we are returning the model object with errors to the view, which will display the error messages.
                 return View("Register", registerDetails);
             }
         }
@@ -64,18 +57,14 @@ namespace RegistrationAndLogin.Controllers
             return View();
         }
 
-        //The login form is posted to this method.
         [HttpPost]
         public ActionResult Login(LoginViewModel model)
         {
-            //Checking the state of model passed as parameter.
             if (ModelState.IsValid)
             {
 
-                //Validating the user, whether the user is valid or not.
                 var isValidUser = IsValidUser(model);
 
-                //If user is valid & present in database, we are redirecting it to Welcome page.
                 if (isValidUser != null)
                 {
                     FormsAuthentication.SetAuthCookie(model.Email, false);
@@ -83,29 +72,23 @@ namespace RegistrationAndLogin.Controllers
                 }
                 else
                 {
-                    //If the username and password combination is not present in DB then error message is shown.
                     ModelState.AddModelError("Failure", "Wrong Username and password combination !");
                     return View();
                 }
             }
             else
             {
-                //If model state is not valid, the model with error message is returned to the View.
                 return View(model);
             }
         }
 
-        //function to check if User is valid or not
         public RegisterUser IsValidUser(LoginViewModel model)
         {
             using (var dataContext = new SqlCodeFirstMvcDbEntities())
             {
-                //Retireving the user details from DB based on username and password enetered by user.
                 RegisterUser user = dataContext.RegisterUsers.Where(query => query.Email.Equals(model.Email) && query.Password.Equals(model.Password)).SingleOrDefault();
-                //If user is present, then true is returned.
                 if (user == null)
                     return null;
-                //If user is not present false is returned.
                 else
                     return user;
             }
