008bc71000a9c0c3cc0ffe5bce298546dc1dfffd
deepalakshmin <deepalakshmi.nakeeran@gmail.com>
2020-11-06 21:55:13 -0800

    sample project to practice jQuery

---
+++ JQueryAjaxSample/JQueryAjaxSample/Controllers/EmployeeController.cs
@@ -0,0 +1,75 @@
+﻿using JQueryAjaxSample.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQueryAjaxSample.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployees());
+        }
+
+        IEnumerable<Employee> GetAllEmployees()
+        {
+            using (DbModel db = new DbModel())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+        }
+
+        public ActionResult AddOrEdit( int id = 0)
+        {
+            Employee emp = new Employee();
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            try
+            {
+                if (emp.ImageUpload != null)
+                {
+                    string filename = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
+                    string extension = Path.GetExtension(emp.ImageUpload.FileName);
+                    filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
+                    emp.ImagePath = "~/AppFiles/Images/" + filename;
+                    emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), filename));
+                }
+                using (DbModel db = new DbModel())
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                }
+                return Json(new
+                {
+                    success = true,
+                    html = Global.RenderRazorViewToString(this, "ViewAll", GetAllEmployees()),
+                    message = "Submitted successfully!"
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new
+                {
+                    success = false,
+                    message = ex.Message
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+
+}
---
+++ JQueryAjaxSample/JQueryAjaxSample/Scripts/Scripts.js
@@ -0,0 +1,49 @@
+﻿function ShowImagePreview(imageUploader, previewImage) {
+    if (imageUploader.files && imageUploader.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(imageUploader.files[0]);
+    }
+}
+
+function jQueryAjaxPost(form) {
+    $.validator.unobtrusive.Parse(form);
+    if ($(form).valid()) {
+        var ajaxConfig = {
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+                if (response.success) {
+                    $("firstTab").html(response.html);
+                    refreshAddNewTab($(form).attr('data-resetUrl'), true);
+                    $.notify(response.message, "success");
+                }
+                else {
+                    $.notify(response.message, "error");
+                }
+            }
+        }
+        if ($(form).attr('enctype') == "multipart/form-data") {
+            ajaxConfig["contentType"] = false;
+            ajaxConfig["processType"] = false;
+        }
+        $.ajax(ajaxConfig);
+    }
+    return false;
+}
+
+function refreshAddNewTab(resetUrl, showViewTab) {
+    $.ajax({
+        type: 'GET',
+        url: reserUrl,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html('Add New');
+            if (showViewTab)
+                $('ul.nav.nav-tabs a:eq(0)').tab('show');
+        }
+    }
+}
